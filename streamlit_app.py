4d95e3ff4208630b92b3e323109d660de9dd48f5
Don10112001 <159415750+Don10112001@users.noreply.github.com>
2024-02-08 15:29:41 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title=" Iris Dashboard",
 )
 
 # Path to the CSV file
-csv_file_path = "C:/Users/Admin/Desktop/Deployment/data/Iris.csv"
+csv_file_path = "Iris.csv"
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(csv_file_path)
